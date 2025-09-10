ee2ff14e2bc78e9363f4ddf40afe2885b77fe69c
Mauricio Herraz <168691108+M4UR1C1O0@users.noreply.github.com>
2025-09-10 23:54:01 +0000

    Prueba

--- Tarea-Data-Science-3/ejercicio4.py
+++ Tarea-Data-Science-3/ejercicio4.py
@@ -1,2 +1,9 @@
 from Limpieza import df
 
+#4. Visualización de datos
+#-------------------------
+#- Haz un histograma de los valores de ataque.
+#- Realiza un gráfico de dispersión entre ataque y velocidad.
+#- Haz un boxplot de los PS por tipo principal (Tipo 1).
+#- Grafica la distribución de la defensa usando un diagrama de violín.
+
