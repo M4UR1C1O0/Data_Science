c20b98c7088845d507f4b188d70b6542954c8b5d
Mauricio Herraz <168691108+M4UR1C1O0@users.noreply.github.com>
2025-09-11 22:14:22 +0000

    Avance

--- Tarea-Data-Science-3/ejercicio7.py
+++ Tarea-Data-Science-3/ejercicio7.py
@@ -13,16 +13,16 @@ import seaborn as sns
 
 #1. ¿Existen tipos de Pokémon que tienden a tener mayor ataque o defensa? Justifica con estadísticas.
 comparativa = df.groupby("Tipo 1")[["Ataque","Defensa"]].agg(["mean","median"]).round(1)#agg para usar las funciones mean y median a la vez
-print(comparativa)
-#si hay tipos de pokemon que pueden tender a tener mayot ataque o defensa, como puede ser el caso de los pokemon de tipo lucha que promedio de ataque es de 102.9 pero una defensa mas baja
-#otro caso seria los pokemon tipo roca que tiene un mayor promedio en la defensa, siendo estos los dos tipos que mas resaltan en defensa y ataque, en cuanto a los demas tipos se puede decir que 
-#tienen un promedio mas balanceado entre ataque y defensa, igualmente tienes el tipo dragon con un buen promedio de ataque y el tipo tierra siendo otro buen tipo en defensa.
+
+'''si hay tipos de pokemon que pueden tender a tener mayot ataque o defensa, como puede ser el caso de los pokemon de tipo lucha que promedio de ataque es de 102.9 pero una defensa mas baja
+otro caso seria los pokemon tipo roca que tiene un mayor promedio en la defensa, siendo estos los dos tipos que mas resaltan en defensa y ataque, en cuanto a los demas tipos se puede decir que 
+tienen un promedio mas balanceado entre ataque y defensa, igualmente tienes el tipo dragon con un buen promedio de ataque y el tipo tierra siendo otro buen tipo en defensa.'''
 
 #2. ¿Hay correlación entre ataque y velocidad? Calcula el coeficiente de correlación.
 correlacion = df["Ataque"].corr(df["Velocidad"])#utilizamos la herramienta de corr para buscar la correlacion entre columnas numericas 
-print(f"El coeficiente de correlacion entre ataque y velocidad es: {correlacion.round(2)}")
-#debido al resultado arrojado es 0.2 se puede decir que no hay una relacion fuerte entre estos ya que esta demasiado cerca del 0 entonces con esto no se puede asegurar que haya una 
-#correlacion entre ataque y velocidad.
+
+'''debido al resultado arrojado es 0.2 se puede decir que no hay una relacion fuerte entre estos ya que esta demasiado cerca del 0 entonces con esto no se puede asegurar que haya una 
+#correlacion entre ataque y velocidad.'''
 
 #3. ¿Qué tan dispersos están los PS dentro de cada tipo? (compara la desviación estándar de PS por tipo)
 #Calculamos la desviacion estandar de PS por tipo y luego creamos un diagrama de dispersion
@@ -37,3 +37,13 @@ plt.tight_layout()#para que no se sobrepongan los textos
 plt.savefig('Dispersión PS por Tipo 1.png')
 
 #4. Identifica posibles outliers en los valores de ataque y PS usando boxplots.#
+plt.figure(figsize=(10,5))
+plt.boxplot([df["Ataque"], df["PS"]], tick_labels=["Ataque", "PS"])
+plt.title('Boxplot de Ataque y PS de Pokemon de la Primera Generacion')
+plt.ylabel('Valores')
+plt.tight_layout()
+plt.savefig('Boxplot Ataque y PS.png')
+
+
+#print(comparativa)
+#print(f"El coeficiente de correlacion entre ataque y velocidad es: {correlacion.round(1)}")
