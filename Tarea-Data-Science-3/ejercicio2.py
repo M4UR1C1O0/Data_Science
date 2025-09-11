67945750a5357a8f46b8a8aeba148e685312ebe0
ElFefoo <117328919+ElFefoo@users.noreply.github.com>
2025-09-11 03:15:15 +0000

    Mejora de comentarios

--- Tarea-Data-Science-3/ejercicio2.py
+++ Tarea-Data-Science-3/ejercicio2.py
@@ -1,5 +1,12 @@
 from Limpieza import df
+#2. Filtrado y selección
+#-----------------------
+#- Filtra todos los Pokémon de tipo "Fuego".
+#- Selecciona solo las columnas Nombre, Tipo 1, Ataque y Velocidad.
 
+#creo un nuevo dataframe con los pokemones de tipo fuego
 tipo_fuego = df[(df["Tipo 1"] == "Fuego") | (df["Tipo 2"] == "Fuego")]
+#selecciono sola las columnas que quiero mostrar
 fuego = tipo_fuego[["Nombre","Tipo 1","Ataque","Velocidad"]]
+#muestro el resultado
 print(fuego)
--- Tarea-Data-Science-3/ejercicio5.py
+++ Tarea-Data-Science-3/ejercicio5.py
@@ -1,20 +1,18 @@
 from Limpieza import df 
-
-
+#5. Manipulación de datos
+#------------------------
+#- Crea una nueva columna llamada "Poder Total" que sea la suma de ataque, defensa, velocidad y PS.
+#- Ordena el DataFrame por "Poder Total" de mayor a menor.
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from Limpieza import df
-
-df_prueba = df.copy()#creo una copia por mientras
-
+#creo una copia del dataframe original para no modificarlo
+df_prueba = df.copy()
+#creo la nueva columna y le ingreso los valores del poder total
 df_prueba["Poder Total"] = df_prueba["Ataque"] + df_prueba["Defensa"] + df_prueba["Velocidad"] + df_prueba["PS"]#agrego la nueva columna que esa la sumatoria
-
-#print(df_prueba[["Nombre", "Poder Total"]])
-
-#ordenar
-
-df_prueba = df_prueba.sort_values(by="Poder Total", ascending=False)#ordeno mayor a menor    
-
-print(df_prueba[["Nombre","Tipo 1", "Tipo 2", "Poder Total"]]) #no puse lo demas porque no alcanza xd
+#ordeno el dataframe utilizando un sort_values usando como parametro el poder total
+df_prueba = df_prueba.sort_values(by="Poder Total", ascending=False) 
+#muestro el resultado del dataframe de prueba
+print(df_prueba[["Nombre","Tipo 1", "Tipo 2", "Poder Total"]]) #para mejor visibilidad omiti los datos de ataque, defensa, velocidad y ps 
 
