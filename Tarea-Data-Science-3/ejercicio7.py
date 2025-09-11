46f06024e7af99894efdd7225eecbbb0e2bcdd59
Mauricio Herraz <168691108+M4UR1C1O0@users.noreply.github.com>
2025-09-11 19:25:19 +0000

    Avance ejercicio7

--- Tarea-Data-Science-3/ejercicio7.py
+++ Tarea-Data-Science-3/ejercicio7.py
@@ -12,13 +12,21 @@ import seaborn as sns
 #- Identifica posibles outliers en los valores de ataque y PS usando boxplots.#
 
 #1. ¿Existen tipos de Pokémon que tienden a tener mayor ataque o defensa? Justifica con estadísticas.
-tipos = ["Normal", "Fuego", "Agua", "Planta", "Electrico", "Hielo",
-    "Lucha", "Veneno", "Tierra", "Volador", "Psiquico",
-    "Bicho", "Roca", "Fantasma", "Dragon",]
-Promedio_ataque_tipo = df.groupby("Tipo 1")["Ataque"].mean().reindex(tipos)
+
 
 #2. ¿Hay correlación entre ataque y velocidad? Calcula el coeficiente de correlación.
 
 #3. ¿Qué tan dispersos están los PS dentro de cada tipo? (compara la desviación estándar de PS por tipo)
+
+#Calculamos la desviacion estandar de PS por tipo y luego creamos un diagrama de dispersion
+Desviacion_estandar_ps = df.groupby("Tipo 1")["PS"].std().round(1)
+plt.figure(figsize=(10,5))
+plt.scatter(df["Tipo 1"], df["PS"], color='green', alpha=0.5)
+plt.title('Dispersión de PS por Tipo 1 de Pokemon de la Primera Generacion')
+plt.xlabel('Tipo 1')
+plt.ylabel('Puntos de Salud (PS)')
+plt.xticks(rotation=45)
+plt.tight_layout()#para que no se sobrepongan los textos
+plt.savefig('Dispersión PS por Tipo 1.png')
 
 #4. Identifica posibles outliers en los valores de ataque y PS usando boxplots.#
